f5400335b9bd43683a1e9f89d8e36a4dfbf537cc
abdullahshaker10 <abdullah.shaker2017@gmail.com>
2018-04-24 15:34:29 +0200

    Add files via upload

---
+++ code_part_5.ino
@@ -0,0 +1,16 @@
+void max_right(int s) {
+  analogWrite(ENA, s);
+  analogWrite(ENB, s);
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, LOW);
+}
+void max_left(int s) {
+  analogWrite(ENA, s);
+  analogWrite(ENB, s);
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+}
