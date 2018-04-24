df0d56ed51423db585a7d65d7d30ff30b846294d
abdallawael <36462421+abdallawael@users.noreply.github.com>
2018-04-24 21:07:34 +0200

    Add files via upload

---
+++ line_follower_part_2.ino
@@ -0,0 +1,21 @@
+void setup() {
+  Serial.begin(9600);
+  //PINMODE SETUP
+  pinMode(trigPin,OUTPUT);
+  //ultrasonic
+  pinMode(echoPin,INPUT);
+  // ultrasonic
+  pinMode (IN1, OUTPUT);
+  pinMode (IN2, OUTPUT);
+  pinMode (IN3, OUTPUT);
+  pinMode (IN4, OUTPUT);
+  pinMode (ENA, OUTPUT);
+  pinMode (ENB, OUTPUT);
+
+ pinMode(sensorleft,INPUT);
+ pinMode(sensorcenter,INPUT);
+ pinMode(sensorright,INPUT);
+
+
+
+}
