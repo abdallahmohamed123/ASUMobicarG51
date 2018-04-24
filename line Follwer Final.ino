35b73f93565f4ee8a63a51f571bdd4842e7b9d44
abdallahmohamed123 <36536663+abdallahmohamed123@users.noreply.github.com>
2018-04-24 18:27:40 +0200

    Add files via upload

---
+++ line Follwer Final.ino
@@ -0,0 +1,155 @@
+
+int IN1 = 8;
+int IN2 = 7;
+int IN3 = 10;
+int IN4 = 11;
+int ENA = 9;
+int ENB = 3;  
+int sensorleft=14 ;
+int sensorcenter= 15;
+int sensorright=16 ;
+int l1=1;
+int l2=1;
+int l3=1;
+
+int f1=0;
+int f2=0;
+int f3=0;
+
+int speed = 200;
+int trigPin = 11;
+int echoPin = 12;
+float lol = 0;
+void setup() {
+  Serial.begin(9600);
+  //PINMODE SETUP
+  pinMode(trigPin,OUTPUT);
+  pinMode(echoPin,INPUT);
+  pinMode (IN1, OUTPUT);
+  pinMode (IN2, OUTPUT);
+  pinMode (IN3, OUTPUT);
+  pinMode (IN4, OUTPUT);
+  pinMode (ENA, OUTPUT);
+  pinMode (ENB, OUTPUT);
+
+ pinMode(sensorleft,INPUT);
+ pinMode(sensorcenter,INPUT);
+ pinMode(sensorright,INPUT);
+
+
+
+}
+
+void forward(int s) {
+  analogWrite(ENA, s);
+  analogWrite(ENB, s);
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+}
+void backward(int s) {
+  analogWrite(ENA, s);
+  analogWrite(ENB, s);
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
+}
+void rotate_right(int s) {
+  analogWrite(ENA, s);
+  analogWrite(ENB, s);
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
+}
+void rotate_left(int s) {
+  analogWrite(ENA, s);
+  analogWrite(ENB, s);
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+}
+void max_right(int s) {
+  analogWrite(ENA, s);
+  analogWrite(ENB, s);
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, LOW);
+}
+void max_left(int s) {
+  analogWrite(ENA, s);
+  analogWrite(ENB, s);
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+}
+void steering_right(int s) {
+  analogWrite(ENA, s);
+  analogWrite(ENB, s/2);
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+}
+void steering_left(int s) {
+  analogWrite(ENA, s/2);
+  analogWrite(ENB, s);
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+}
+void steady() {
+  analogWrite(ENA, 0);
+  analogWrite(ENB, 0);
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, LOW);
+}
+
+void rotate_left_us(int speed) {
+  max_left(speed);
+}
+void rotate_right_us(int speed) {
+  max_right(speed);
+}
+
+void loop() {
+l1=digitalRead(sensorleft);
+l2=digitalRead(sensorcenter);
+l3=digitalRead(sensorright);
+
+
+if(l1==1&&l2==1&&l3==1)   //feedback when comes all sensor on white
+{
+void  steady() ;
+l1=f1;
+l2=f2;
+l3=f3;
+}
+
+if(l1==0&&l2==1&&l3==1||l1==0&&l2==0&&l3==1)   //left turn
+{
+ rotate_left(speed) ; 
+}
+else
+if(l1==1&&l2==1&&l3==0||l1==1&&l2==0&&l3==0)  //right turn
+{
+  rotate_right(speed) ;
+}
+else
+if(l1==1&&l2==0&&l3==1)  
+{
+ forward(speed);
+}
+
+f1=l1;
+f2=l2;
+f3=l3;        //memory variables
+}
