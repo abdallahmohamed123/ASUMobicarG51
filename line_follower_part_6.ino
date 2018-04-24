590d4a25a1a0a773d98ab3cbbd8cc23cf379f18d
Hagar-Essam <36584159+Hagar-Essam@users.noreply.github.com>
2018-04-24 21:09:51 +0200

    Add files via upload

---
+++ line_follower_part_6.ino
@@ -0,0 +1,16 @@
+void steering_right(int s) {
+  analogWrite(ENA, s);
+  analogWrite(ENB, s/2);
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+}
+void steering_left(int s) {
+  analogWrite(ENA, s/2);
+  analogWrite(ENB, s);
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+}
