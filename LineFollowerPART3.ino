182f9b4c3a6675ce35e50b9307bbf70a54e1a6eb
HabibaAyman96 <36582124+HabibaAyman96@users.noreply.github.com>
2018-04-24 16:27:40 +0200

    Add files via upload

---
+++ LineFollowerPART3.ino
@@ -0,0 +1,18 @@
+void forward(int s){
+  analogWrite(ENA, s);
+  analogWrite(ENB, s);
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+}
+void backward(int s) {
+  analogWrite(ENA, s);
+  analogWrite(ENB,s);
+  digitalWrite(IN1,LOW);
+  digitalWrite(IN2,HIGH);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4,HIGH);
+}
+  
+
