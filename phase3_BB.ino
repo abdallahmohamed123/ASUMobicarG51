38d6b79586fa901ae478ae35bf5e73ffb691dd83
HabibaAyman96 <36582124+HabibaAyman96@users.noreply.github.com>
2018-07-06 23:52:44 +0200

    Add files via upload

---
+++ phase3_BB.ino
@@ -0,0 +1,129 @@
+if(z==0){
+
+  
+analogWrite(ENA, 155);
+  analogWrite(ENB, 155);
+    
+        digitalWrite(AF,HIGH);
+    digitalWrite(BF,HIGH);
+    digitalWrite(AB,LOW);
+    digitalWrite(BB,LOW);
+    delay(k);
+
+           analogWrite(ENA, 155);
+  analogWrite(ENB, 155);
+   digitalWrite(AF,LOW);
+    digitalWrite(BF,HIGH);
+    digitalWrite(AB,HIGH);
+    digitalWrite(BB,LOW);
+    delay( angledelay.toFloat());
+
+           analogWrite(ENA, 155);
+  analogWrite(ENB, 155);
+        digitalWrite(AF,HIGH);
+    digitalWrite(BF,HIGH);
+    digitalWrite(AB,LOW);
+    digitalWrite(BB,LOW);
+    delay(k);
+
+           analogWrite(ENA, 155);
+  analogWrite(ENB, 155);
+   digitalWrite(AF,LOW);
+    digitalWrite(BF,HIGH);
+    digitalWrite(AB,HIGH);
+    digitalWrite(BB,LOW);
+    delay( angledelay.toFloat());
+
+           analogWrite(ENA, 155);
+  analogWrite(ENB, 155);
+        digitalWrite(AF,HIGH);
+    digitalWrite(BF,HIGH);
+    digitalWrite(AB,LOW);
+    digitalWrite(BB,LOW);
+    delay(k);
+    
+           analogWrite(ENA, 155);
+  analogWrite(ENB, 155);
+   digitalWrite(AF,LOW);
+    digitalWrite(BF,HIGH);
+    digitalWrite(AB,HIGH);
+    digitalWrite(BB,LOW);
+    delay( angledelay.toFloat());
+    
+           analogWrite(ENA, 115);
+  analogWrite(ENB, 115);
+        digitalWrite(AF,HIGH);
+    digitalWrite(BF,HIGH);
+    digitalWrite(AB,LOW);
+    digitalWrite(BB,LOW);
+    delay(k);
+
+           analogWrite(ENA, 115);
+  analogWrite(ENB, 115);
+   digitalWrite(AF,LOW);
+    digitalWrite(BF,HIGH);
+    digitalWrite(AB,HIGH);
+    digitalWrite(BB,LOW);
+    delay( angledelay.toFloat());
+
+  analogWrite(ENA, 255);//circle
+  analogWrite(ENB, 83);
+  digitalWrite(AF, HIGH);
+  digitalWrite(AB, LOW);
+  digitalWrite(BF, HIGH);
+  digitalWrite(BB, LOW);
+  delay(4000);
+  
+    
+
+  analogWrite(ENA, 255);//circle
+  analogWrite(ENB, 83);
+  digitalWrite(AF, HIGH);
+  digitalWrite(AB, LOW);
+  digitalWrite(BF, HIGH);
+  digitalWrite(BB, LOW);
+  delay(2000);
+  
+
+  analogWrite(ENA, 80);//circle
+  analogWrite(ENB, 255);
+  digitalWrite(AF, HIGH);
+  digitalWrite(AB, LOW);
+  digitalWrite(BF, HIGH);
+  digitalWrite(BB, LOW);
+  delay(3000);
+
+
+
+    
+    digitalWrite(AF,LOW);
+    digitalWrite(BF,LOW);
+    digitalWrite(AB,LOW);
+    digitalWrite(BB,LOW);
+
+z=2;
+}
+break;
+
+
+   
+         default:
+    digitalWrite(AF,LOW);
+    digitalWrite(BF,LOW);
+    digitalWrite(AB,LOW);
+    digitalWrite(BB,LOW);      
+         
+         
+} 
+  }
+  }
+     
+   
+  //car make 90 degree in 926 mel second
+  //car make 65 cm in 10000 mel second
+  //batterys at 12.2 v#include <SoftwareSerial.h>
+SoftwareSerial bluetooth (10,11);//Rx,Tx (ARDUINO)
+char data ;
+#define trigPin 9
+#defin
+
